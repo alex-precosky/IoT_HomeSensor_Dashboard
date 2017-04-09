be63158bea0d74ef093da7de454c29a33fc2af78
Alex Precosky <Alex Precosky>
2017-04-09 11:28:31 -0700

    Removed old commented out bits from the wsgi passenger file

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -7,12 +7,4 @@ sys.path.insert(0, "/home/precosky/homesensors.alexwarrior.cc/WSGISQLPlot")
 sys.path.insert(0, "/home/precosky/homesensors.alexwarrior.cc/FridgeAPI")
 
 import fridgeapi
-#import plotApp
-
 from fridgeapi import app as application
-#from plotApp import app as application
-
-#def application(environ, start_response):
-#    start_response('200 OK', [('Content-type', 'text/html')])
-#    return plotApp.getResponse()
-
